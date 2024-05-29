73fa4d795d95888683e147481b41e7a67ffabd18
canisminor <canisminor2023@outlook.com>
2024-05-29 11:32:34 +0800

    20240529

--- webapps/servlet-demo1/src/org/canisminor/servlet/Servlet1.java
+++ webapps/servlet-demo1/src/org/canisminor/servlet/Servlet1.java
@@ -1,6 +1,7 @@
 package org.canisminor.servlet;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -8,6 +9,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+@WebServlet("/servlet1")
 public class Servlet1 extends HttpServlet {
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
