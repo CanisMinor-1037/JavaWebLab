5b64547e16c9706bf527e806ab28c8985ee43a49
canisminor <canisminor2023@outlook.com>
2024-05-29 17:51:16 +0800

    20240529

--- webapps/demo3-servletconfig/src/org/canisminor/servlet/Servlet1.java
+++ webapps/demo3-servletconfig/src/org/canisminor/servlet/Servlet1.java
@@ -1,6 +1,25 @@
 package org.canisminor.servlet;
 
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.Enumeration;
 
 public class Servlet1 extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ServletConfig servletConfig = getServletConfig();
+        // 获取所有初始参数名
+        Enumeration<String> initParameterNames = servletConfig.getInitParameterNames();
+
+        // 根据参数名获取参数值
+        while (initParameterNames.hasMoreElements()) {
+            String parameterName = initParameterNames.nextElement();
+            System.out.println(parameterName + ": " + servletConfig.getInitParameter(parameterName));
+        }
+    }
 }
--- webapps/demo3-servletconfig/src/org/canisminor/servlet/Servlet3.java
+++ webapps/demo3-servletconfig/src/org/canisminor/servlet/Servlet3.java
@@ -1,2 +1,36 @@
-package org.canisminor.servlet;public class Servlet3 {
+package org.canisminor.servlet;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebInitParam;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.Enumeration;
+
+@WebServlet(
+        value = "/servlet3",
+        initParams = {
+                @WebInitParam(name = "k1", value = "v11"),
+                @WebInitParam(name = "k2", value = "v22"),
+                @WebInitParam(name = "k3", value = "v33"),
+                @WebInitParam(name = "k4", value = "v44")
+        }
+)
+public class Servlet3 extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ServletConfig servletConfig = getServletConfig();
+        // 获取所有初始参数名
+        Enumeration<String> initParameterNames = servletConfig.getInitParameterNames();
+
+        // 根据参数名获取参数值
+        while (initParameterNames.hasMoreElements()) {
+            String parameterName = initParameterNames.nextElement();
+            System.out.println(parameterName + ": " + servletConfig.getInitParameter(parameterName));
+        }
+    }
 }
